44dc8e91bb69b76d0a66484535d69f97fc7737c1
Rohit Kumar Bindal <rohit.k.bindal@intimetec.com>
2022-07-28 13:57:52 +0530

    Todo functionality Complete: Using Data Binding

--- week-1/todo-app/src/app/todo-using-data-binding/todo-db-item/todo-db-item.component.ts
+++ week-1/todo-app/src/app/todo-using-data-binding/todo-db-item/todo-db-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { TODO } from "src/app/todo.model";
 
 @Component({
@@ -13,13 +13,37 @@ export class TodoDataBindingItemComponent implements OnInit {
   * 'todo' will not be undefined.
   */
   @Input() todo!: TODO;
+
+  // Creating CUstom events to manage the todo deletion and update operations
+  @Output() updateTodo = new EventEmitter<TODO>();
+  @Output() deleteTodo = new EventEmitter<TODO>();
+
   todoStatus = 'pending';
   // An array to store different status to be used in a <select> element.
   statusOptions: [string, string, string] = ['pending', 'inprogress', 'completed'];
   // Set the default selector as 'pending'
   statusSelected: string = this.statusOptions[0];
+  // Activate Edit mode
+  isEditable = false;
+
   constructor() { }
+
   ngOnInit(): void {
+    // Initialize the todoStatus
     this.todoStatus = this.todo.status.toLowerCase();
+  }
+
+  onEditClicked() {
+    // If there is a change update the todo list.
+    if (this.isEditable) {
+      // Emit an updateTodo event to notify the parent component that a todo has been updated/edited.
+      this.updateTodo.emit(this.todo);
+    }
+    this.isEditable = !this.isEditable;
+  }
+
+  onDeleteClicked() {
+    // Emit a deleteTodo event to notify the parent component that a todo has been deleted.
+    this.deleteTodo.emit(this.todo);
   }
 }
--- week-1/todo-app/src/app/todo-using-data-binding/todo-db.component.ts
+++ week-1/todo-app/src/app/todo-using-data-binding/todo-db.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { TODO } from "../todo.model";
 import { TODO_DATA } from "./todos.data";
 
@@ -7,8 +7,27 @@ import { TODO_DATA } from "./todos.data";
   styleUrls: ['./todo-db.component.css'],
   templateUrl: './todo-db.component.html',
 })
-export class TodoDataBindingComponent {
+export class TodoDataBindingComponent implements OnInit {
   // Set the todos data and create a copy.
-  todos: TODO[] | undefined = TODO_DATA.slice();
+  todos: TODO[] | undefined;
+  constructor() { }
 
+  ngOnInit(): void {
+    // Initialize the lists of Todos.
+    this.todos = TODO_DATA.slice();
+  }
+
+  // This function is triggered when a deleteTodo event is fired on the child component
+  onTodoDelete(event: TODO) {
+    // Ask for a confirmation
+    const confirmation = confirm('Are you sure want to delete ' + event.text)
+    if (confirmation)
+      this.todos!.splice(this.todos!.indexOf(event), 1);
+  }
+
+  // This function is triggered when a updateTodo event is fired on the child component
+  onTodoUpdated(event: TODO) {
+    const todoIndex = this.todos!.indexOf(event);
+    this.todos![todoIndex] = event;
+  }
 }
