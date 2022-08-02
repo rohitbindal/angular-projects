700a62c8ff96ca94263a7d71ac644b50a8309ee0
Rohit Kumar Bindal <rohit.k.bindal@intimetec.com>
2022-08-02 13:26:55 +0530

    Project Setup

--- week-2-dashboard-app/src/app/app.module.ts
+++ week-2-dashboard-app/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ObservablesComponent } from './observables/observables.component';
+import { ErrorHandlingComponent } from './error-handling/error-handling.component';
+import { DependencyInjectionComponent } from './dependency-injection/dependency-injection.component';
+import { BindingComponent } from './binding/binding.component';
+import { ServicesComponent } from './services-component/services.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServicesComponent,
+    ObservablesComponent,
+    ErrorHandlingComponent,
+    DependencyInjectionComponent,
+    BindingComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
