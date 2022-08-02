94c9ba433db2568917aaffc9269633b4e82297a5
Rohit Kumar Bindal <rohit.k.bindal@intimetec.com>
2022-08-02 14:07:08 +0530

    Added basic navigation, added header

--- week-2-dashboard-app/src/app/app.component.ts
+++ week-2-dashboard-app/src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+// Defining a screen status type.
+type ScreenStatus = {
+  [id: string]: {
+    status: boolean
+  }
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'week-2-dashboard-app';
+  // Variable to store the current screen status.
+  currentScreen: ScreenStatus;
+
+  constructor() {
+    // Initializing the current screen status
+    this.currentScreen =
+    {
+      'binding': {
+        status: true
+      },
+      'services': {
+        status: false,
+      },
+      'dependencyInjection': {
+        status: false,
+      },
+      'observables': {
+        status: false,
+      },
+      'errorHandling': {
+        status: false,
+      }
+    }
+  }
+
+  // Change the boolean value denoting the screen status when the button is clicked.
+  onStatusChange(id: string) {
+    for (const prop in this.currentScreen) {
+      // Activate/Deactivate the respective screen
+      if (prop === id) {
+        this.currentScreen[id].status = !this.currentScreen[id].status;
+      }
+      // Deactivate all the other screens.
+      else {
+        this.currentScreen[prop].status = false;
+      }
+    }
+  }
 }
