6a61a0cb039982852c766f429838154824790968
Rohit Kumar Bindal <rohit.k.bindal@intimetec.com>
2022-08-09 15:23:29 +0530

    Http operations, interceptors

--- angular-practice/http-01-start/src/app/app.component.ts
+++ angular-practice/http-01-start/src/app/app.component.ts
@@ -1,28 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { Post } from "./models/post.model";
+import { PostsService } from "./services/posts.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent implements OnInit {
-  loadedPosts = [];
+export class AppComponent implements OnInit, OnDestroy {
+  loadedPosts: Post[] = [];
+  isFetching = false;
+  error: string | null = null;
+  private errorSubscription?: Subscription;
+  constructor(private _postsService: PostsService) {}
 
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {}
+  ngOnInit() {
+    this.errorSubscription = this._postsService.error.subscribe((error) => {
+      this.error = error;
+    });
+    this.onFetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    this._postsService.createAndStorePost(postData.title, postData.content);
   }
 
   onFetchPosts() {
     // Send Http request
+    this.isFetching = true;
+    this._postsService.fetchPosts().subscribe(
+      (posts) => {
+        this.loadedPosts = posts;
+        this.isFetching = false;
+      },
+      (error) => {
+        this.error = error.message;
+      }
+    );
   }
 
   onClearPosts() {
     // Send Http request
+    this._postsService.deleteAllPosts().subscribe(() => {
+      this.loadedPosts = [];
+    });
+  }
+
+  onHandleError() {
+    this.error = null;
+    this.isFetching = false;
+  }
+
+  ngOnDestroy(): void {
+    this.errorSubscription?.unsubscribe();
   }
 }
