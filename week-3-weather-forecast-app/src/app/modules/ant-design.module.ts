8b1fead18e238c9c11b2b0c3f16d4ae904e26b58
Rohit Kumar Bindal <rohit.k.bindal@intimetec.com>
2022-08-12 19:26:25 +0530

    Removed Week-3 Assignment

--- week-3-weather-forecast-app/src/app/modules/ant-design.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { NzCardModule } from 'ng-zorro-antd/card';
-
-@NgModule({
-  imports: [],
-  exports: [NzCardModule],
-})
-export class AntDesignModule {}
