f76643f59724bd51270eef782f01dc9b90833be1
Rohit Kumar Bindal <rohit.k.bindal@intimetec.com>
2022-08-16 09:41:20 +0530

    Added API Key

--- week-3-weather-forecast-app/src/environments/environment.ts
+++ week-3-weather-forecast-app/src/environments/environment.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_KEY: '98e345f353d64ad4acc55648220308',
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
