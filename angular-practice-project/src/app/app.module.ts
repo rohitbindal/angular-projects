3d11c09d0e372332bba8049db9098c2f416c545e
Rohit Kumar Bindal <rohit.k.bindal@intimetec.com>
2022-08-17 12:23:12 +0530

    Added firebase database to store recipes

--- angular-practice-project/src/app/app.module.ts
+++ angular-practice-project/src/app/app.module.ts
@@ -1,7 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -30,7 +30,13 @@ import { ShoppingService } from './shopping-list/shopping-list.service';
     RecipeStartComponent,
     RecipeEditComponent,
   ],
-  imports: [BrowserModule, FormsModule, AppRoutingModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
   providers: [ShoppingService],
   bootstrap: [AppComponent],
 })
--- angular-practice-project/src/app/header/header.component.ts
+++ angular-practice-project/src/app/header/header.component.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private _dataStorageService: DataStorageService) {}
+  onSaveData() {
+    this._dataStorageService.storeRecipes();
+  }
+  onFetchData() {
+    this._dataStorageService.fetchRecipes().subscribe();
+  }
+}
