e9fe80d24a6f86922824038c92f3d9b9b216ed75
Rohit Kumar Bindal <rohit.k.bindal@intimetec.com>
2022-08-30 11:46:41 +0530

    Added links to the header

--- week-5-final-project-ecommerce-app/src/app/main/components/header/header.component.ts
+++ week-5-final-project-ecommerce-app/src/app/main/components/header/header.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { HELPERS } from '../../../shared/constants/helpers';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  helpers = HELPERS;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
