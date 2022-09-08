28182579efb988b2d096b6b5eba437a1abd1c141
Rohit Kumar Bindal <rohit.k.bindal@intimetec.com>
2022-09-08 18:51:38 +0530

    Fixed Subscriber guard, fixed product detail toasts

--- week-5-final-project-ecommerce-app/src/app/main/components/products-detail/products-detail.component.ts
+++ week-5-final-project-ecommerce-app/src/app/main/components/products-detail/products-detail.component.ts
@@ -2,9 +2,13 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { APP_ROUTES } from '../../../shared/constants/app-routes';
+import { User } from '../../../shared/constants/authorization.model';
+import { HELPERS } from '../../../shared/constants/helpers';
 import { Product } from '../../../shared/constants/product.model';
+import { FirebaseAuthService } from '../../../shared/services/firebase/auth.firebase.service';
 import { FirebaseDataService } from '../../../shared/services/firebase/data.firebase.service';
 import { ProductService } from '../../../shared/services/product.service';
+import { ToastService } from '../../../shared/services/toast.service';
 
 @Component({
   selector: 'app-products-detail',
@@ -13,20 +17,69 @@ import { ProductService } from '../../../shared/services/product.service';
 })
 export class ProductsDetailComponent implements OnInit, OnDestroy {
   product: Product | null;
-
+  currentUser: User | null;
   private productDetail$: Subscription | null;
 
   constructor(
     private _products: ProductService,
     private _route: ActivatedRoute,
     private _router: Router,
-    private _data: FirebaseDataService
+    private _data: FirebaseDataService,
+    private _auth: FirebaseAuthService,
+    private _toast: ToastService
   ) {
     this.product = null;
     this.productDetail$ = null;
+    this.currentUser = null;
   }
 
   ngOnInit(): void {
+    this._auth.user.subscribe((user) => {
+      if (user) {
+        this.currentUser = user;
+      }
+    });
+    this.updateUI();
+  }
+
+  // Add the product to the cart
+  onAddToCartClicked() {
+    if (this.currentUser && !this.currentUser.disabled)
+      this._data.addProductToCart(this.product!);
+    else if (this.currentUser && this.currentUser.disabled)
+      this._toast.showErrorToast(HELPERS.errors.ACCOUNT_DISABLED_BY_ADMIN);
+    else
+      this._toast.showErrorToast(HELPERS.errors.ACCOUNT_NEEDED_TO_ADD_TO_CART);
+  }
+
+  // Add to cart and navigate to checkout
+  onBuyNowClicked() {
+    if (this.currentUser && !this.currentUser.disabled) {
+      this._data.addProductToCart(this.product!);
+      this._router.navigate([APP_ROUTES.absolute.main.cart]).then();
+    } else if (this.currentUser && this.currentUser.disabled)
+      this._toast.showErrorToast(HELPERS.errors.ACCOUNT_DISABLED_BY_ADMIN);
+    else
+      this._toast.showErrorToast(HELPERS.errors.ACCOUNT_NEEDED_TO_ADD_TO_CART);
+  }
+
+  // Add the product to user wishlist
+  onWishlistClicked() {
+    if (this.currentUser && !this.currentUser.disabled)
+      this._data.addProductToWishlist(this.product!);
+    else if (this.currentUser && this.currentUser.disabled)
+      this._toast.showErrorToast(HELPERS.errors.ACCOUNT_DISABLED_BY_ADMIN);
+    else
+      this._toast.showErrorToast(
+        HELPERS.errors.ACCOUNT_NEEDED_TO_ADD_TO_WISHLIST
+      );
+  }
+
+  ngOnDestroy() {
+    this.productDetail$?.unsubscribe();
+  }
+
+  private updateUI() {
     const id = this._route.snapshot.params['id'];
     this.productDetail$ = this._products
       .getProductById(id)
@@ -36,25 +89,5 @@ export class ProductsDetailComponent implements OnInit, OnDestroy {
           this._router.navigate([APP_ROUTES.absolute.pageNotFound]).then();
         }
       });
-  }
-
-  // Add the product to the cart
-  onAddToCartClicked() {
-    this._data.addProductToCart(this.product!);
-  }
-
-  // Add to cart and navigate to checkout
-  onBuyNowClicked() {
-    this._data.addProductToCart(this.product!);
-    this._router.navigate([APP_ROUTES.absolute.main.cart]).then();
-  }
-
-  // Add the product to user wishlist
-  onWishlistClicked() {
-    this._data.addProductToWishlist(this.product!);
-  }
-
-  ngOnDestroy() {
-    this.productDetail$?.unsubscribe();
   }
 }
